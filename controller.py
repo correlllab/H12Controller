1672a3a08cfd8f8d85ccdfc26a250dbbb0bc5022
humanoid desktop <you@example.com>
2025-04-22 11:18:32 -0600

    velocity control

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ from robot_control.inspire_hand import H1HandController
 
 from scipy.spatial.transform import Rotation as R
 
-from pose_constraints import start_left, start_right, in_safety_cylinder
+from pose_constraints import start_left, start_right, in_safety_cylinder, out_of_range
 import cv2
 
 def rpy_xyz_to_matrix( x, y, z, roll, pitch, yaw):
@@ -37,7 +37,8 @@ class ControllerWrapper():
         self.left_arm = start_left  # [x, y, z, roll, pitch, yaw]
         self.right_arm = start_right
         self.max_velocity = 0.5  # meters per second
-        self.step_hz = 100  # control loop frequency (Hz)
+        self.step_hz = 50  # control loop frequency (Hz)
+        self.loop_dt = 1.0 / self.step_hz
 
         self.set_arms_pose(*self.left_arm, *self.right_arm)
         self.set_fingers(np.ones(6), np.ones(6))
@@ -60,6 +61,12 @@ class ControllerWrapper():
         if in_safety_cylinder(Rx, Ry, Rz):
             print(f"Right pose in safety cylinder {Rx=}, {Ry=}, {Rz=}")
             return
+        if out_of_range(Lx, Ly, Lz, arm="left"):
+            print(f"Left pose out of range {Lx=}, {Ly=}, {Lz=}")
+            return
+        if out_of_range(Rx, Ry, Rz, arm="right"):
+            print(f"Right pose out of range {Rx=}, {Ry=}, {Rz=}")
+            return
 
         steps = max(self.n_steps(Lx, Ly, Lz, "left"), self.n_steps(Rx, Ry, Rz, "right"))
 
@@ -67,6 +74,7 @@ class ControllerWrapper():
         start_right_arm = self.right_arm.copy()
 
         for i in range(steps + 1):
+            loop_start = time.perf_counter()
             alpha = i / steps
 
             current_L = [
@@ -100,7 +108,63 @@ class ControllerWrapper():
 
             self.arm_ctrl.ctrl_dual_arm(sol_q, sol_tauff)
             #print(f"Step {i}/{steps}: Left Arm: {self.left_arm}, Right Arm: {self.right_arm}")
-            time.sleep(1.0 / self.step_hz)
+            elapsed = time.perf_counter() - loop_start
+            sleep_time = self.loop_dt - elapsed
+            time.sleep(max(0, sleep_time))
+        return
+    
+    def set_arms_velocity(self, vLx, vLy, vLz, vLroll, vLpitch, vLyaw, vRx, vRy, vRz, vRroll, vRpitch, vRyaw, total_time=1):
+        start_time = time.time()
+        v_left = [vLx, vLy, vLz, vLroll, vLpitch, vLyaw]
+        v_right = [vRx, vRy, vRz, vRroll, vRpitch, vRyaw]
+        while time.time() - start_time < total_time:
+            loop_start = time.perf_counter()
+            current_L = [self.left_arm[j] + (v_left[j] * self.loop_dt) for j in range(len(self.left_arm)) ]
+            current_R = [self.right_arm[j] + (v_right[j] * self.loop_dt) for j in range(len(self.right_arm)) ]
+            if in_safety_cylinder(current_L[0], current_L[1], current_L[2]):
+                print(f"Left pose in safety cylinder {current_L[0]=}, {current_L[1]=}, {current_L[2]=}")
+                return
+            if in_safety_cylinder(current_R[0], current_R[1], current_R[2]):
+                print(f"Right pose in safety cylinder {current_R[0]=}, {current_R[1]=}, {current_R[2]=}")
+                return
+            if out_of_range(current_L[0], current_L[1], current_L[2], arm="left"):
+                print(f"Left pose out of range {current_L[0]=}, {current_L[1]=}, {current_L[2]=}")
+                return
+            if out_of_range(current_R[0], current_R[1], current_R[2], arm="right"):
+                print(f"Right pose out of range {current_R[0]=}, {current_R[1]=}, {current_R[2]=}")
+            self.left_arm = current_L
+            self.right_arm = current_R
+
+            constructed_l_target = rpy_xyz_to_matrix(*self.left_arm)
+            constructed_r_target = rpy_xyz_to_matrix(*self.right_arm)
+
+            current_lr_arm_q = self.arm_ctrl.get_current_dual_arm_q()
+            current_lr_arm_dq = self.arm_ctrl.get_current_dual_arm_dq()
+
+            sol_q, sol_tauff = self.arm_ik.solve_ik(
+                constructed_l_target,
+                constructed_r_target,
+                current_lr_arm_q,
+                current_lr_arm_dq
+            )
+
+            if sol_q is None or sol_tauff is None:
+                print(f"IK failed at step {i}")
+                break
+
+            self.arm_ctrl.ctrl_dual_arm(sol_q, sol_tauff)
+
+            elapsed = time.perf_counter() - loop_start
+            sleep_time = self.loop_dt - elapsed
+            time.sleep(max(0, sleep_time))
+    def set_arm_velocity(self, vx, vy, vz, vroll, vpitch, vyaw, arm="left", total_time=1):
+        if arm == "left":
+            self.set_arms_velocity(vx, vy, vz, vroll, vpitch, vyaw, 0,0,0,0,0,0, total_time=total_time)
+        elif arm == "right":
+            self.set_arms_velocity(0,0,0,0,0,0, vx, vy, vz, vroll, vpitch, vyaw, total_time=total_time)
+        else:
+            raise ValueError("Invalid arm specified. Use 'left' or 'right'.")
+            
 
     def set_arm_pose(self, x, y, z, r, p, yaw, arm="left"):
         if in_safety_cylinder(x, y, z):
@@ -140,7 +204,7 @@ if __name__ == '__main__':
             print(f"Failed to load image from {image_path}")
 
         cv2.createTrackbar("X", "Arm Control", 500, 1000, nothing)
-        cv2.createTrackbar("Y", "Arm Control", 700, 1000, nothing)
+        cv2.createTrackbar("Y", "Arm Control", 500, 1000, nothing)
         cv2.createTrackbar("Z", "Arm Control", 500, 1000, nothing)
         cv2.createTrackbar("Roll", "Arm Control", 0, 360, nothing)
         cv2.createTrackbar("Pitch", "Arm Control", 0, 360, nothing)
@@ -159,7 +223,7 @@ if __name__ == '__main__':
 
             # Set the arm pose using the values
             
-            controller.set_arm_pose(x, y, z, roll, pitch, yaw, arm=arm)
+            controller.set_arm_velocity(x, y, z, roll, pitch, yaw, arm=arm)
 
             # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
--- pose_constraints.py
+++ pose_constraints.py
@@ -1,3 +1,4 @@
+import numpy as np
 start_left = [0.1, 0.2, 0, 0.0, 0.0, 0.0]
 start_right = [0.1, -0.2, 0, 0.0, 0.0, 0.0]
 basic_pose_L = Lx, Ly, Lz, Lroll, Lpitch, Lyaw = 0.373, 0.323, 0.223, 0.0, 0.5899991834424116, 0.0
@@ -13,3 +14,14 @@ def in_safety_cylinder(x, y, z):
     within_height = -height <= z <= height
 
     return within_radius and within_height
+
+def out_of_range(x,y,z, arm):
+    sphere_center = np.array([0, 0.148, 0.423])
+    if arm == "left":
+        sphere_center[1] *= 1
+    elif arm == "right":
+        sphere_center[1] *= -1
+    sphere_radius = 0.70
+    point = np.array([x, y, z])
+    distance = np.linalg.norm(point - sphere_center)
+    return distance > sphere_radius
